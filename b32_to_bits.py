6c518c081b58cb56a7ac9fe6bbea6b5ca1999d9f
GAFAがえるさん <code@gafafrog.com>
2025-08-03 16:20:53 -0700

    Update b32 script to make output more human friendly

--- b32_to_bits.py
+++ b32_to_bits.py
@@ -17,9 +17,18 @@ def base32_to_bit_array(b32_string):
     return bit_string
 
 
+def readable_array(arr, chunk_size):
+    '''
+    読みやすくなるように間にスペースを入れる
+    '''
+    chunks = [arr[i:i+chunk_size] for i in range(0, len(arr), chunk_size)]
+    return " ".join(chunks)
+
+
 if __name__ == '__main__':
     iam_id_part = sys.argv[1]
     bit_array = base32_to_bit_array(iam_id_part)
+    array_as_str = readable_array(bit_array, 5)
 
     print(f"Base32文字列: {iam_id_part}")
-    print(f"ビット配列: {bit_array}")
+    print(f"ビット配列: {array_as_str}")
